ab86ca1ca47cffcf5107a553b294859a330dae4d
autumn <1060590974@qq.com>
2024-01-25 14:23:31 +0800

    加入动画效果

--- src/views/index.vue
+++ src/views/index.vue
@@ -492,20 +492,29 @@
           </el-tab-pane>
         </el-tabs>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" v-if="!isAdmin && (form.state == null)" @click="submitForm('1')">保 存</el-button>
-        <el-button type="primary" v-if="!isAdmin && (form.state == '1' || form.state == null)" @click="submitForm('2')">
-          提 交
+      <div class="dialog-footer">
+        <el-button class="btnPublic" type="primary" v-if="!isAdmin && (form.state == null)" @click="submitForm('1')">保
+          存
         </el-button>
-        <el-button type="primary" v-if="isAdmin && (form.state == '3' || form.state == '2')" @click="submitForm('4')">通
+        <button class="button btnPublic" @click="animateButtonClick"
+                v-if="!isAdmin && (form.state == '1' || form.state == null)">
+          <span class="default">提交</span>
+          <span class="success">提交成功</span>
+          <div class="left"></div>
+          <div class="right"></div>
+        </button>
+        <el-button class="btnPublic" type="primary" v-if="isAdmin && (form.state == '3' || form.state == '2')"
+                   @click="submitForm('4')">通
           过
         </el-button>
-        <el-button type="primary" v-if="!isAdmin && (form.state == '4')" @click="submitForm('5')">已 读</el-button>
-        <el-button type="primary" v-if="!isAdmin && (form.state == '5') && form.scoreIsNull"
+        <el-button class="btnPublic" type="primary" v-if="!isAdmin && (form.state == '4')" @click="submitForm('5')">已
+          读
+        </el-button>
+        <el-button class="btnPublic" type="primary" v-if="!isAdmin && (form.state == '5') && form.scoreIsNull"
                    @click="submitForm('5')">评 价
         </el-button>
-        <el-button type="primary" v-if="isAdmin" @click="submitForm(null)">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
+        <el-button class="btnPublic" type="primary" v-if="isAdmin" @click="submitForm(null)">确 定</el-button>
+        <el-button class="btnPublic" @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
   </div>
@@ -519,8 +528,9 @@ import {
   addUserFillInfo,
   updateUserFillInfo
 } from "@/api/vaccines/userFillInfo";
-import "@/assets/elementStyle/fonts/style.css";
 import {getVaccinationInfoByMapToLV} from "@/api/vaccines/vaccinesInfo";
+import {gsap} from "gsap";
+import "@/assets/send/style.css";
 
 
 export default {
@@ -653,6 +663,139 @@ export default {
     this.getVaccinationMethodByMapToLV();
   },
   methods: {
+    animateButtonClick() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          document.querySelectorAll(".button").forEach((button) => {
+            let getVar = (variable) => getComputedStyle(button).getPropertyValue(variable);
+            if (!button.classList.contains("active")) {
+              button.classList.add("active");
+              gsap.to(button, {
+                keyframes: [
+                  // 动画关键帧（同提供的脚本中的内容）
+                  {
+                    "--left-wing-first-x": 50,
+                    "--left-wing-first-y": 100,
+                    "--right-wing-second-x": 50,
+                    "--right-wing-second-y": 100,
+                    duration: 0.2,
+                    onComplete() {
+                      gsap.set(button, {
+                        "--left-wing-first-y": 0,
+                        "--left-wing-second-x": 40,
+                        "--left-wing-second-y": 100,
+                        "--left-wing-third-x": 0,
+                        "--left-wing-third-y": 100,
+                        "--left-body-third-x": 40,
+                        "--right-wing-first-x": 50,
+                        "--right-wing-first-y": 0,
+                        "--right-wing-second-x": 60,
+                        "--right-wing-second-y": 100,
+                        "--right-wing-third-x": 100,
+                        "--right-wing-third-y": 100,
+                        "--right-body-third-x": 60,
+                      });
+                    },
+                  },
+                  {
+                    "--left-wing-third-x": 20,
+                    "--left-wing-third-y": 90,
+                    "--left-wing-second-y": 90,
+                    "--left-body-third-y": 90,
+                    "--right-wing-third-x": 80,
+                    "--right-wing-third-y": 90,
+                    "--right-body-third-y": 90,
+                    "--right-wing-second-y": 90,
+                    duration: 0.2,
+                  },
+                  {
+                    "--rotate": 50,
+                    "--left-wing-third-y": 95,
+                    "--left-wing-third-x": 27,
+                    "--right-body-third-x": 45,
+                    "--right-wing-second-x": 45,
+                    "--right-wing-third-x": 60,
+                    "--right-wing-third-y": 83,
+                    duration: 0.25,
+                  },
+                  {
+                    "--rotate": 55,
+                    "--plane-x": -8,
+                    "--plane-y": 24,
+                    duration: 0.2,
+                  },
+                  {
+                    "--rotate": 40,
+                    "--plane-x": 45,
+                    "--plane-y": -180,
+                    "--plane-opacity": 0,
+                    duration: 0.3,
+                    onComplete() {
+                      setTimeout(() => {
+                        button.removeAttribute("style");
+                        gsap.fromTo(
+                            button,
+                            {
+                              opacity: 0,
+                              y: -8,
+                            },
+                            {
+                              opacity: 1,
+                              y: 0,
+                              clearProps: true,
+                              duration: 0.3,
+                              onComplete() {
+                                button.classList.remove("active");
+                              },
+                            }
+                        );
+                      }, 2000);
+                    },
+                  }
+                ],
+                onComplete: () => {
+                  // 在动画完成后执行的代码
+                  this.animationComplete();
+                },
+              });
+              gsap.to(button, {
+                keyframes: [
+                  // 动画关键帧（同提供的脚本中的内容）
+                  {
+                    "--text-opacity": 0,
+                    "--border-radius": 0,
+                    "--left-wing-background": getVar("--primary-darkest"),
+                    "--right-wing-background": getVar("--primary-darkest"),
+                    duration: 0.1,
+                  },
+                  {
+                    "--left-wing-background": getVar("--primary"),
+                    "--right-wing-background": getVar("--primary"),
+                    duration: 0.1,
+                  },
+                  {
+                    "--left-body-background": getVar("--primary-dark"),
+                    "--right-body-background": getVar("--primary-darkest"),
+                    duration: 0.4,
+                  },
+                  {
+                    "--success-opacity": 1,
+                    "--success-scale": 1,
+                    duration: 0.25,
+                    delay: 0.25,
+                  },
+                ],
+              });
+            }
+          });
+        }
+      });
+    },
+    animationComplete() {
+      // 在动画完成后执行的逻辑
+      console.log('动画执行完毕！');
+      this.submitForm('2');
+    },
     myEvent() {
       console.log("事件被触发了！");
       // 在这里添加你想要执行的代码
@@ -942,5 +1085,16 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.05);
+}
+
+.dialog-footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+
+  .btnPublic {
+    margin-left: 10px;
+  }
+
 }
 </style>
